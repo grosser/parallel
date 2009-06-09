f48a1e38f5fb7fcceefc109d5a6e926ebff3a2b2
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2009-06-09 08:12:26 +0200

    Missed a space :).

--- lib/parallel_specs.rb
+++ lib/parallel_specs.rb
@@ -21,7 +21,7 @@ module ParallelSpecs
   end
 
   def run_tests(test_files, process_number)
-    cmd = "export RAILS_ENV=test ; export TEST_ENV_NUMBER=#{process_number == 0 ? '' :process_number + 1} ; export RSPEC_COLOR=1 ; script/spec -O spec/spec.opts #{test_files * ' '}"
+    cmd = "export RAILS_ENV=test ; export TEST_ENV_NUMBER=#{process_number == 0 ? '' : process_number + 1} ; export RSPEC_COLOR=1 ; script/spec -O spec/spec.opts #{test_files * ' '}"
     f = open("|#{cmd}")
     while out = f.gets(".")
       print out
