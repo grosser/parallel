9d5b7bbdc879a4b033c7f804bd3d3dbe14ae16b1
grosser <grosser.michael@gmail.com>
2011-06-03 08:09:40 +0200

    support windows for number_of_processes

--- lib/parallel.rb
+++ lib/parallel.rb
@@ -70,6 +70,11 @@ class Parallel
       `cat /proc/cpuinfo | grep processor | wc -l`.to_i
     when /freebsd/
       `sysctl -n hw.ncpu`.to_i
+    when /mswin|mingw/
+      require 'win32ole'
+      wmi = WIN32OLE.connect("winmgmts://")
+      cpu = wmi.ExecQuery("select NumberOfCores from Win32_Processor") # TODO count hyper-threaded in this
+      cpu.to_enum.first.NumberOfCores
     end
   end
 
