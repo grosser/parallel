7d7fda52d1e4417e83f76bd931c0dc4224b09589
jmozmoz <Joachim.Herb@gmx.de>
2013-03-29 18:28:52 +0100

    Removed incorrect begin/rescue block from test cases

--- spec/cases/map_with_processes_and_return.rb
+++ spec/cases/map_with_processes_and_return.rb
@@ -1,13 +1,10 @@
 require File.expand_path('spec/spec_helper')
 
-begin
-  Parallel.map(1..100, :in_processes => 4) do |x|
-    sleep 0.1 # so all processes get started
-    print x
-    return if x == 1
-    sleep 0.1 # so no now work gets queued before exception is raised
-    x
-  end
-rescue
-  print ' returned'
+Parallel.map(1..100, :in_processes => 4) do |x|
+  sleep 0.1 # so all processes get started
+  print x
+  return if x == 1
+  sleep 0.1 # so no now work gets queued before exception is raised
+  x
 end
+print ' returned'
