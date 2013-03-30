fef3b5443d474cfcac1dfab53a1ae66d5c592740
jmozmoz <Joachim.Herb@gmx.de>
2013-03-30 12:42:50 +0100

    Use break to exit all threads

--- spec/cases/map_with_processes_and_break.rb
+++ spec/cases/map_with_processes_and_break.rb
@@ -3,8 +3,8 @@ require File.expand_path('spec/spec_helper')
 Parallel.map(1..100, :in_processes => 4) do |x|
   sleep 0.1 # so all processes get started
   print x
-  return if x == 1
-  sleep 0.1 # so no now work gets queued before exception is raised
+  break if x == 1
+  sleep 0.1 # so no now work gets queued before break is executed
   x
 end
-print ' returned'
+print ' broke'
