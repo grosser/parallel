f8dc1d65ddb5037e7b340f6ca6f15df181656f85
jmozmoz <Joachim.Herb@gmx.de>
2013-03-30 23:44:06 +0100

    Use Parallel::Break exception in both threads and processes.

--- spec/cases/map_with_processes_and_break.rb
+++ spec/cases/map_with_processes_and_break.rb
@@ -3,8 +3,8 @@ require File.expand_path('spec/spec_helper')
 Parallel.map(1..100, :in_processes => 4) do |x|
   sleep 0.1 # so all processes get started
   print x
-  return if x == 1
-  sleep 0.1 # so no now work gets queued before return is executed
+  raise Parallel::Break if x == 1
+  sleep 0.1 # so no now work gets queued before Parallel::Break is raised
   x
 end
-print ' returned'
+print ' Parallel::Break raised'
