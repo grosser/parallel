2e57effe7d6cba9d229fb7d11fd1cbc23bb8a880
Ralf Mueller <ralf.mueller@zmaw.de>
2016-01-30 13:56:39 +0100

    use child class instead of Queue extension

--- lib/parallel/queue.rb
+++ lib/parallel/queue.rb
@@ -4,24 +4,28 @@ require 'thread'
 #         suggestions from Robert Klemme (https://www.ruby-forum.com/topic/68001#86298)
 #
 # ==============================================================================
-include Parallel::ProcessorCount
 
-class Queue
-  alias :qpush :push
+class ParallelQueue < Queue
+  alias :queue_push :push
+  include Parallel::ProcessorCount
+
   def push (*item, &block)
-    qpush(item   ) unless item.empty?
-    qpush([block]) unless block.nil?
+    queue_push(item   ) unless item.empty?
+    queue_push([block]) unless block.nil?
   end
   def run(workers=processor_count)
-    qpush(Parallel::Stop)
+    queue_push(Parallel::Stop)
     Parallel.map(self,:in_threads => workers) {|task|
       if task.size > 1
         if task[0].kind_of? Proc
-          # Expects proc/lambda with arguments, e.g. [mysqrt,2.789]
+          # Expects proc/lambda with arguments,e.g.
+          # [mysqrt,2.789]
+          # [myproc,x,y,z]
           task[0].call(*task[1..-1])
         else
           # expect an object in task[0] and one of its methods with arguments in task[1] as a symbol
-          # e.g. [a,[:attribute=,1]
+          # e.g. [a,[:attribute=,1] or
+          # Math,:exp,0
           task[0].send(task[1],*task[2..-1])
         end
       else
