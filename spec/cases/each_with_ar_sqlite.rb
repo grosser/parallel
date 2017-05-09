d3967839c03319fb59bc5352550017e9bba6745d
Michael Grosser <michael@grosser.it>
2017-05-09 14:57:21 -0700

    use Tempfile.open for automatic cleanup

--- spec/cases/each_with_ar_sqlite.rb
+++ spec/cases/each_with_ar_sqlite.rb
@@ -4,37 +4,35 @@ require "sqlite3"
 STDOUT.sync = true
 in_worker_type = "in_#{ENV.fetch('WORKER_TYPE')}".to_sym
 
-temp = Tempfile.new("db")
+Tempfile.open("db") do |temp|
 
-ActiveRecord::Schema.verbose = false
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => temp.path
-)
+  ActiveRecord::Schema.verbose = false
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => temp.path
+  )
 
-class User < ActiveRecord::Base
-end
+  class User < ActiveRecord::Base
+  end
 
-# create tables
-unless User.table_exists?
-  ActiveRecord::Schema.define(:version => 1) do
-    create_table :users do |t|
-      t.string :name
+  # create tables
+  unless User.table_exists?
+    ActiveRecord::Schema.define(:version => 1) do
+      create_table :users do |t|
+        t.string :name
+      end
     end
   end
+
+  User.delete_all
+
+  3.times { User.create!(:name => "X") }
+
+  puts "Parent: #{User.first.name}"
+
+  Parallel.each([1], in_worker_type => 1) do
+    puts "Parallel (#{in_worker_type}): #{User.all.map(&:name).join}"
+  end
+
+  puts "Parent: #{User.first.name}"
 end
-
-User.delete_all
-
-3.times { User.create!(:name => "X") }
-
-puts "Parent: #{User.first.name}"
-
-Parallel.each([1], in_worker_type => 1) do
-  puts "Parallel (#{in_worker_type}): #{User.all.map(&:name).join}"
-end
-
-puts "Parent: #{User.first.name}"
-
-temp.close
-temp.unlink
