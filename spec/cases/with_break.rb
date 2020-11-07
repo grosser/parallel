4c16b355eca801ff00598e509306c24eeb6c2338
Michael Grosser <michael@grosser.it>
2020-11-06 19:51:45 -0800

    allow breaking with value

--- spec/cases/with_break.rb
+++ spec/cases/with_break.rb
@@ -8,7 +8,7 @@ worker_size = (ENV['WORKER_SIZE'] || 4).to_i
 result = Parallel.public_send(method, 1..100, in_worker_type => worker_size) do |x|
   sleep 0.1 # so all workers get started
   print x
-  raise Parallel::Break if x == 1
+  raise Parallel::Break, *ARGV if x == 1
   sleep 0.2 # so now no work gets queued before Parallel::Break is raised
   x
 end
