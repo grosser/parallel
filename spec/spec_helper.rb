499e8387a3625be1db143faebbf34a81e559193a
Michael Grosser <michael@grosser.it>
2021-11-18 22:22:51 -0800

    Make sure to run test cases with target Ruby version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,11 @@ RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :should }
   config.mock_with(:rspec) { |c| c.syntax = :should }
   config.around { |example| Timeout.timeout(30, &example) }
+  config.include(
+    Module.new do
+      def ruby(cmd)
+        `#{RbConfig.ruby} #{cmd}`
+      end
+    end
+  )
 end
