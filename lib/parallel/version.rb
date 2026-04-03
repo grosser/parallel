090b054b25fd5c1bb2f61b6f25e2cd385510ae2d
Michael Grosser <michael@grosser.it>
2026-04-02 19:12:40 -0700

    v2.0.0

--- lib/parallel/version.rb
+++ lib/parallel/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module Parallel
-  VERSION = Version = '1.28.0' # rubocop:disable Naming/ConstantName
+  VERSION = Version = '2.0.0' # rubocop:disable Naming/ConstantName
 end
