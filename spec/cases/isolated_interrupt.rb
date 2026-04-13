559643cc2fcb244adcbe82f45b2c60cdd6c76f84
Peter Winton <pwinton@indigobio.com>
2026-04-13 18:49:46 -0400

    Add description to isolate_interrupt.rb

--- spec/cases/isolated_interrupt.rb
+++ spec/cases/isolated_interrupt.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 require './spec/cases/helper'
 
+# Queue two items to be processed in a single worker with isolation enabled.
+# Shortly after work begins on the second item, send the process the interrupt signal.
+# The process should terminate nearly immediately, indicating the replacement process
+# was killed rather than completing work on the second item.
+
 parent_pid = Process.pid
 killer_pid = fork do
   sleep 1
